39e7c69653c3dc1c13464b151f055e6297e9a0de
Alena Moravova <moraval2@fel.cvut.cz>
2015-10-19 12:25:39 +0200

    upload new version

---
+++ Cuda/CWChangeWeightsKernel.cu
@@ -0,0 +1,154 @@
+//Includes for IntelliSense 
+#define _SIZE_T_DEFINED
+
+#include <cuda.h>
+#include <device_launch_parameters.h>
+#include <texture_fetch_functions.h>
+#include "float.h"
+#include <builtin_types.h>
+#include <vector_functions.h>
+#include <math.h>
+
+
+
+extern "C"
+{
+	__constant__ int D_INPUT_UNITS;
+	__constant__ int D_HIDDEN_UNITS;
+	__constant__ int D_OUTPUT_UNITS;
+	__constant__ int D_NEURONS_PER_GROUP;
+	__constant__ int D_NEURON_GROUPS;
+
+	__global__ void CWChangeInputWeightsKernel(
+		float *inputWeights,
+		float *inputWeightDeltas,
+		float *outputWeights,
+		float *outputDeltas,
+		float *inputWeightRTRLDerivatives,
+
+		float trainingRate,
+		float momentum,
+
+		int *activeGroups,
+		int contextByActivations
+		)
+	{
+		int weightId = blockDim.x*blockIdx.y*gridDim.x	//rows preceeding current row in grid
+			+ blockDim.x*blockIdx.x				//blocks preceeding current block
+			+ threadIdx.x;
+
+		int unitID = weightId / D_INPUT_UNITS;
+		int groupID = unitID / D_NEURONS_PER_GROUP;
+
+		extern __shared__ float activeGroupsShared[];
+
+		if (weightId < D_NEURON_GROUPS)
+		{
+			activeGroupsShared[weightId] = activeGroups[weightId];
+		}
+		__syncthreads();
+
+		if (weightId < D_HIDDEN_UNITS * D_INPUT_UNITS 
+			&& (contextByActivations || activeGroupsShared[groupID] == 1))
+		{
+			float gradient = 0;
+
+			for (int i = 0; i < D_OUTPUT_UNITS; i++)
+			{
+				float sum = 0;
+				for (int j = 0; j < D_HIDDEN_UNITS; j++)
+				{
+					sum += outputWeights[i * D_HIDDEN_UNITS + j] * inputWeightRTRLDerivatives[j * D_HIDDEN_UNITS * D_INPUT_UNITS + weightId];
+				}
+
+				gradient += outputDeltas[i] * sum;
+			}
+
+			float weightDelta = trainingRate * gradient + momentum * inputWeightDeltas[weightId];
+			inputWeightDeltas[weightId] = weightDelta;
+			inputWeights[weightId] += weightDelta;
+		}
+	}
+
+	__global__ void CWChangeRecurrentWeightsKernel(
+		float *recurrentWeights,
+		float *recurrentWeightDeltas,
+		float *outputWeights,
+		float *outputDeltas,
+		float *recurrentWeightRTRLDerivatives,
+
+		float trainingRate,
+		float momentum,
+
+		int *activeGroups,
+		int contextByActivations
+		)
+	{
+		int weightId = blockDim.x*blockIdx.y*gridDim.x	//rows preceeding current row in grid
+			+ blockDim.x*blockIdx.x				//blocks preceeding current block
+			+ threadIdx.x;
+
+		int unitID = weightId / D_HIDDEN_UNITS;
+		int groupID = unitID / D_NEURONS_PER_GROUP;
+		int x = weightId - (unitID * D_HIDDEN_UNITS);
+
+		extern __shared__ float activeGroupsShared[];
+
+		if (weightId < D_NEURON_GROUPS)
+		{
+			activeGroupsShared[weightId] = activeGroups[weightId];
+		}
+		__syncthreads();
+
+		if (weightId < D_HIDDEN_UNITS * D_HIDDEN_UNITS 
+			&& (contextByActivations || activeGroupsShared[groupID])
+			&& (contextByActivations || (x >= (groupID * D_NEURONS_PER_GROUP))))		
+		{
+			// parallel reduction?
+			float gradient = 0;
+
+			for (int i = 0; i < D_OUTPUT_UNITS; i++)
+			{
+				float sum = 0;
+				for (int j = 0; j < D_HIDDEN_UNITS; j++)
+				{
+					sum += outputWeights[i * D_HIDDEN_UNITS + j] * recurrentWeightRTRLDerivatives[j * D_HIDDEN_UNITS * D_HIDDEN_UNITS + weightId];
+				}
+
+				gradient += outputDeltas[i] * sum;
+			}
+
+			float weightDelta = trainingRate * gradient + momentum * recurrentWeightDeltas[weightId];
+			recurrentWeightDeltas[weightId] = weightDelta;
+			//recurrentWeightDeltas[weightId] = 1000;
+			recurrentWeights[weightId] += weightDelta;
+			//recurrentWeights[weightId] += 1000;
+		}
+	}
+
+	__global__ void CWChangeOutputWeightsKernel(
+		float *outputWeights,
+		float *outputWeightDeltas,
+		float *outputDeltas,
+		float *hiddenActivations,
+
+		float trainingRate,
+		float momentum
+		)
+	{
+		int weightId = blockDim.x*blockIdx.y*gridDim.x	//rows preceeding current row in grid
+			+ blockDim.x*blockIdx.x				//blocks preceeding current block
+			+ threadIdx.x;
+
+		int to = weightId / D_HIDDEN_UNITS;
+		int from = weightId % D_HIDDEN_UNITS;
+
+		if (weightId < D_OUTPUT_UNITS * D_HIDDEN_UNITS)
+		{
+			float gradient = outputDeltas[to] * hiddenActivations[from];
+			float weightDelta = trainingRate * gradient + momentum * outputWeightDeltas[weightId];
+			outputWeightDeltas[weightId] = weightDelta;
+			outputWeights[weightId] += weightDelta;
+		}
+	}
+}
---
+++ Cuda/CWFeedForwardKernel.cu
@@ -0,0 +1,98 @@
+//Includes for IntelliSense 
+#define _SIZE_T_DEFINED
+
+#include <cuda.h>
+#include <device_launch_parameters.h>
+#include <texture_fetch_functions.h>
+#include "float.h"
+#include <builtin_types.h>
+#include <vector_functions.h>
+#include <math.h>
+
+#include "C:\Users\Alka\Disk Google\fel\SummerCamp\BrainSimulator-master\Sources\Modules\BasicNodes\Cuda\NeuralNetwork\Activation/ActivationFunction.cu"
+
+
+extern "C"
+{
+	__constant__ int D_INPUT_UNITS;
+	__constant__ int D_HIDDEN_UNITS;
+	__constant__ int D_OUTPUT_UNITS;
+	__constant__ int D_NEURONS_PER_GROUP;
+	__constant__ int D_NEURON_GROUPS;
+	__constant__ ActivationFunctionEnum D_ACTIVATION_FUNCTION;
+
+	__global__ void CWFeedforwardHiddenKernel(
+		float *input, 
+		float *hiddenActivations,
+		float *previousHiddenActivations, 
+		float *hiddenActivationDerivatives, 
+		float *inputWeights, 
+		float *recurrentWeights,
+		int *activeGroups
+		)
+	{
+		int unitId = blockDim.x*blockIdx.y*gridDim.x	//rows preceeding current row in grid
+			+ blockDim.x*blockIdx.x				//blocks preceeding current block
+			+ threadIdx.x;
+
+		extern __shared__ float activeGroupsShared[];
+
+		if (unitId < D_NEURON_GROUPS)
+		{
+			activeGroupsShared[unitId] = activeGroups[unitId];
+		}
+		__syncthreads();
+
+		int groupID = unitId / D_NEURONS_PER_GROUP;
+
+		if (unitId < D_HIDDEN_UNITS && activeGroupsShared[groupID] == 1)
+		{
+			int weightId = unitId * D_INPUT_UNITS;
+
+			float weightedSum = 0;
+			for (int i = 0; i < D_INPUT_UNITS; i++)
+			{
+				weightedSum += inputWeights[weightId] * input[i];
+				weightId++;
+			}
+
+			weightId = unitId * D_HIDDEN_UNITS;
+
+			for (int i = 0; i < D_HIDDEN_UNITS; i++)
+			{
+				weightedSum += recurrentWeights[weightId] * previousHiddenActivations[i];
+				weightId++;
+			}
+			
+			// dodelat synchronizaci vlaken, aby se mohlo pouzivat jen jedno pole hidden activations
+			//__syncthreads();
+
+			hiddenActivations[unitId] = Evaluate(D_ACTIVATION_FUNCTION, weightedSum);
+			hiddenActivationDerivatives[unitId] = EvaluateDerivative(D_ACTIVATION_FUNCTION, weightedSum);
+		}
+	}
+
+	__global__ void CWFeedforwardOutputKernel(float *hiddenActivations, float *outputActivations, float *outputActivationDerivatives, float *outputWeights)
+	{
+		int unitId = blockDim.x*blockIdx.y*gridDim.x	//rows preceeding current row in grid
+			+ blockDim.x*blockIdx.x				//blocks preceeding current block
+			+ threadIdx.x;
+
+		if (unitId < D_OUTPUT_UNITS)
+		{
+			
+			int weightId = unitId * D_HIDDEN_UNITS;
+
+			float weightedSum = 0;
+			for (int i = 0; i < D_HIDDEN_UNITS; i++)
+			{
+				weightedSum += outputWeights[weightId] * hiddenActivations[i];
+				weightId++;
+			}
+
+			outputActivations[unitId] = Evaluate(D_ACTIVATION_FUNCTION, weightedSum);
+			
+			outputActivationDerivatives[unitId] = EvaluateDerivative(D_ACTIVATION_FUNCTION, weightedSum);
+		}
+	}
+}
